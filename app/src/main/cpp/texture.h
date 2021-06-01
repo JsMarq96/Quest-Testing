27cfc51727e1c6b297909acb759c9147cfc785a8
Juan S. Marquerie <50079201+JsMarq96@users.noreply.github.com>
2021-06-01 17:31:42 +0200

    Added textures with cubemap support

---
+++ app/src/main/cpp/texture.h
@@ -0,0 +1,34 @@
+//
+// Created by Juan S. Marquerie on 29/05/2021.
+//
+
+#ifndef QUEST_DEMO_TEXTURE_H
+#define QUEST_DEMO_TEXTURE_H
+
+#include <GLES3/gl3.h>
+#include <string.h>
+
+#define DEFAULT_TEXT_FIDELITY 0
+
+struct sTexture {
+    bool             store_on_RAM = false;
+    bool             is_cube_map  = false;
+
+    // Raw data
+    int             width     = 0;
+    int             height    = 0;
+    int             layers    = 0;
+    unsigned char   *raw_data = NULL;
+
+    // OpenGL id
+    unsigned int     texture_id;
+};
+
+void init_texture(sTexture  *text,
+                  const bool is_cube_map,
+                  const bool store_on_RAM,
+                  const char *texture_name);
+
+void delete_texture(sTexture  *text);
+
+#endif //QUEST_DEMO_TEXTURE_H
